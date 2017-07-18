b0c5893db20620fc043c3d14ee4487bab64abc75
eminkaygisiz96 <eminkaygisiz96@googlemail.com>
2017-07-18 23:39:03 +0200

    Update dummyServices.js

--- finale abgabe/dienstgeber/rest/dummyServices.js
+++ finale abgabe/dienstgeber/rest/dummyServices.js
@@ -1,18 +1,7 @@
 'use strict';
 
 exports.register = function(app) {
-
-    app.get('/login/securityquestions', function (req, res) {
-        res.json([
-            "Dummy Question 1",
-            "Dummy Question 2"
-        ]);
-    })
-
-    app.options('/login', function(req, res) {
-        res.send('POST');
-    })
-
+    
     app.get('/login', function(req, res) {
         res.sendStatus(405)
     })
@@ -26,16 +15,5 @@ exports.register = function(app) {
             res.send("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ");
         }
     })
-
-    app.get('/menu', function (req, res) {
-        res.json([{"text": "Home","link": "/home"}]);
-    })
-
-    app.get('/properties', function (req, res) {
-        res.json([{}]);
-    })
-
-    app.get('/login/logout', function(req, res) {
-        res.send(true)
     })
 }
